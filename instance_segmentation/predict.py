175b7decc60315b1ae3f79921473e6153a1129c5
Lucas Caputi <lcaputi77@gmail.com>
2024-02-27 10:06:30 -0800

    Added instance segmentation

---
+++ instance_segmentation/predict.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+from PIL import Image, ImageDraw
+import cv2
+import numpy as np
+
+# Image and model paths
+model_path = '/home/lcaputi/temp/image-segmentation-yolov8/last.pt'
+image_path = '/home/lcaputi/temp/image-segmentation-yolov8/test_image.jpg'
+
+# Read the image
+img = cv2.imread(image_path)
+
+# Load a model
+model = YOLO(model_path)
+
+# Run batched inference on image
+results = model.predict(img)  # return a list of Results objects
+
+# Process results list
+for result in results:
+    # OUTLINE OUTPUT
+    masks = result.masks  # Masks object for segmentation masks outputs
+    for mask in masks:
+        polygon = mask.xy[0]                                            # Get polygon of mask
+        img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # Convert the image from OpenCV format (BGR) to Pillow format (RGB)
+        draw = ImageDraw.Draw(img_pil)                                  # Create an ImageDraw object
+        draw.polygon(polygon, outline=(0, 255, 0), width=5)             # Draw the polygon outline with given color and thickness
+        img = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)        # Convert the modified image back to OpenCV format (BGR)
+    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))     # Convert the image from OpenCV format (BGR) to PIL format (RGB)
+    output_image_path = '/home/lcaputi/temp/image-segmentation-yolov8/output_image.jpg'
+    img_pil.save(output_image_path)    
+
+    # NORMAL OUTPUT
+    # boxes = result.boxes                        # Boxes object for bounding box outputs
+    # masks = result.masks                        # Masks object for segmentation masks outputs
+    # keypoints = result.keypoints                # Keypoints object for pose outputs
+    # probs = result.probs                        # Probs object for classification outputs
+    # result.show()                               # display to screen
+    # result.save(filename='output_image.jpg')    # save to disk
+
