598551fecce79b3c58624d3ae09f1cd5bc4959cb
Noah Masten <nmasten25@gmail.com>
2024-03-06 14:30:34 -0800

    Added mobilenet model

--- MobileApp/components/NewRouteScreen.js
+++ MobileApp/components/NewRouteScreen.js
@@ -1,33 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Image, Alert } from "react-native"; // Import Alert
 import * as ImagePicker from "expo-image-picker";
 import { styles } from "./Styles";
+import { classifyUsingMobilenet } from "./TensorFlowModel";
 
 export function NewRouteScreen() {
+  
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [result, setResult] = useState('');
+
   const [permission, requestPermission] = ImagePicker.useCameraPermissions();
   const [imageUri, setImageUri] = useState(null);
 
   const takeImage = async () => {
-    let data = await ImagePicker.launchCameraAsync({
+    let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
     });
-    if (!data.cancelled) {
-      setImageUri(data.assets[0].uri);
-      console.log(data.assets[0].uri);
+    if (!result.cancelled) {
+      setImageUri(result.assets[0].uri);
+      console.log(result.assets[0].uri);
     }
   };
 
   const pickImage = async () => {
-    let data = await ImagePicker.launchImageLibraryAsync({
+    let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       quality: 1,
     });
-    if (!data.cancelled) {
-      setImageUri(data.assets[0].uri);
-      console.log(data.assets[0].uri);
+    if (!result.cancelled) {
+      setImageUri(result.assets[0].uri);
+      console.log(result.assets[0].uri);
     }
   };
+
+
+
+useEffect(() => {
+    classifyUsingMobilenet(imageUri, setIsTfReady, setResult);
+  }, [imageUri]);
 
   return (
     <View style={styles.container}>
@@ -41,9 +52,24 @@ export function NewRouteScreen() {
       <TouchableOpacity style={styles.button} onPress={takeImage}>
         <Text style={styles.buttonText}>Take an image</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={pickImage}>
+      {isTfReady && <TouchableOpacity style={styles.button} onPress={pickImage}>
         <Text style={styles.buttonText}>Upload an image</Text>
-      </TouchableOpacity>
+      </TouchableOpacity>}
+      {!isTfReady && (
+        <Text style={{ width: 300, textAlign: "center" }}>
+          Loading TFJS model...
+        </Text>
+      )}
+      {isTfReady && result === "" && (
+        <Text style={{ width: 300, textAlign: "center" }}>
+          Select an image to classify
+        </Text>
+      )}
+      {result !== "" && (
+        <Text style={{ width: 300, textAlign: "center" }}>
+          {result}
+        </Text>
+      )}
     </View>
   );
 }
